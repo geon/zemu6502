d43b3376456a940b1bc5b3a2a39d739aebbc4d3a
Victor Widell <victor.widell@gmail.com>
2025-12-10 21:13:21 +0100

    Updated build.zig version 0.12 -> 0.15

--- build.zig
+++ build.zig
@@ -23,13 +23,13 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "zemu6502",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{
-            .path = "src/main.zig",
-        },
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            // In this case the main source file is merely a path, however, in more
+            // complicated build scripts, this could be a generated file.
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
     exe.root_module.addImport("yaml", yaml_pkg.module("yaml"));
 
@@ -64,11 +64,11 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const unit_tests = b.addTest(.{
-        .root_source_file = .{
-            .path = "src/main.zig",
-        },
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
     unit_tests.root_module.addImport("yaml", yaml_pkg.module("yaml"));
 
