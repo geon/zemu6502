500693ff7e2bdd4ec7d8c005c7544566570aba69
Victor Widell <victor.widell@gmail.com>
2026-06-24 22:14:01 +0200

    Removed config.

--- src/devices.zig
+++ src/devices.zig
@@ -1,71 +1,13 @@
 //! Hardware devices
 
 const std = @import("std");
-const BusAddressConfig = @import("config.zig").BusAddressConfig;
-const SystemConfig = @import("config.zig").SystemConfig;
 const Peripheral = @import("peripheral.zig");
 
 pub const builtin = @import("devices/builtin.zig");
 
-/// Definition of all devices.
-const Device = enum {
-    const Self = @This();
-
-    // Builtin devices
-    ram,
-
-    fn fromString(name: []const u8) ?Self {
-        return std.meta.stringToEnum(Self, name);
-    }
-};
-
-const DeviceError = error{
-    UnknownDevice,
-    ImageNotFound,
-};
-
-/// Create a peripheral device from a config entry.
-pub fn createDevice(allocator: std.mem.Allocator, system_dir: std.fs.Dir, config: *const BusAddressConfig, system_config: *const SystemConfig) !Peripheral {
-    _ = system_config;
-    const device_config = config.peripheral;
-    const device = Device.fromString(device_config.type) orelse {
-        std.log.err("Unknown device type: {s}", .{device_config.type});
-        return DeviceError.UnknownDevice;
-    };
-
-    var peripheral = switch (device) {
-        .ram => (try builtin.RAM.init(allocator, config.size())).peripheral(),
-    };
-
-    std.log.info(
-        "Initialised device {s} - {s}",
-        .{ peripheral.vtable.name, peripheral.vtable.description },
-    );
-
-    // Load binary file.
-    if (device_config.load) |image_path| {
-        std.log.info(
-            "Loading {s} into {s}",
-            .{ image_path, peripheral.vtable.name },
-        );
-
-        const MAX_IMAGE_SIZE: usize = 0x10000;
-
-        // Load initial rom bin
-        if (system_dir.openFile(image_path, .{})) |file| {
-            const buffer = try file.readToEndAlloc(allocator, MAX_IMAGE_SIZE);
-            defer allocator.free(buffer);
-            try peripheral.load(buffer);
-        } else |err| switch (err) {
-            error.FileNotFound => {
-                std.log.err("Initial peripheral image not found: {s}", .{image_path});
-            },
-            error.FileTooBig => {
-                std.log.err("File {s} exceeds maximum images size {}bytes", .{ image_path, MAX_IMAGE_SIZE });
-            },
-            else => return err,
-        }
-    }
+/// Create a peripheral device.
+pub fn createDevice(allocator: std.mem.Allocator) !Peripheral {
+    const peripheral = (try builtin.RAM.init(allocator, 0xffff)).peripheral();
 
     return peripheral;
 }
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const config = @import("config.zig");
 const devices = @import("devices.zig");
 const System = @import("system.zig");
 
@@ -11,47 +10,13 @@ pub const std_options: std.Options = .{
     .log_level = .info,
 };
 
-/// Stupidly simple command line arguments
-const Args = struct {
-    config_file: []const u8,
-};
-
-fn processArgs(allocator: Allocator) !Args {
-    const args = try std.process.argsAlloc(allocator);
-    if (args.len != 2) {
-        var stderr_buffer: [1024]u8 = undefined;
-        var stderr_writer = std.fs.File.stderr().writer(&stderr_buffer);
-        const stderr = &stderr_writer.interface;
-
-        try stderr.print(
-            "Missing config file argument.\n{s} CONFIG_FILE",
-            .{args[0]},
-        );
-        return error.Unimplemented;
-    }
-
-    return .{ .config_file = args[1] };
-}
-
-fn createPeripherals(allocator: Allocator, system_dir: std.fs.Dir, system: *System, system_config: config.SystemConfig) !void {
-    for (system_config.dataBus) |bus_address_config| {
-        const peripheral = try devices.createDevice(allocator, system_dir, &bus_address_config, &system_config);
-        try system.data_bus.addPeripheral(.{
-            .start = bus_address_config.start,
-            .end = bus_address_config.end,
-            .peripheral = peripheral,
-        });
-        std.log.info(
-            "Added {s} to bus at @{X:0^4}-{X:0^4}",
-            .{ peripheral.vtable.name, bus_address_config.start, bus_address_config.end },
-        );
-    }
-}
-
-/// Clone of the method from std library to return a sentenal
-pub fn realpathAlloc(self: std.fs.Dir, allocator: Allocator, pathname: []const u8) ![:0]u8 {
-    var buf: [std.fs.max_path_bytes]u8 = undefined;
-    return allocator.dupeZ(u8, try self.realpath(pathname, buf[0..]));
+fn createPeripherals(allocator: Allocator, system: *System) !void {
+    const peripheral = try devices.createDevice(allocator);
+    try system.data_bus.addPeripheral(.{
+        .start = 0,
+        .end = 0xffff,
+        .peripheral = peripheral,
+    });
 }
 
 /// Main entry point
@@ -60,35 +25,13 @@ pub fn main() !void {
     defer arena.deinit();
     const allocator = arena.allocator();
 
-    // Parse command line and load system config
-    const args = processArgs(allocator) catch |err| switch (err) {
-        error.Unimplemented => return,
-        else => return err,
-    };
-    const config_path = std.fs.realpathAlloc(allocator, args.config_file) catch |err| switch (err) {
-        error.FileNotFound => {
-            std.log.err("Config file not found: {s}", .{args.config_file});
-            return;
-        },
-        else => return err,
-    };
-    defer allocator.free(config_path);
+    const clockFreq = 10;
 
-    // Resolve system working dir (relative to config file)
-    var system_dir: std.fs.Dir = undefined;
-    if (std.fs.path.dirname(config_path)) |path| {
-        system_dir = try std.fs.openDirAbsolute(path, .{});
-    } else {
-        system_dir = std.fs.cwd();
-    }
-
-    const system_config = try config.from_file(allocator, config_path);
-
-    // Create system and add devices defined in config.
-    var system = try System.init(allocator, system_config.clockFreq);
+    // Create system and add devices.
+    var system = try System.init(allocator, clockFreq);
     defer system.deinit();
-    std.log.info("Initialised system @ {d}Hz", .{system_config.clockFreq});
-    try createPeripherals(allocator, system_dir, &system, system_config);
+    std.log.info("Initialised system @ {d}Hz", .{clockFreq});
+    try createPeripherals(allocator, &system);
 
     system.reset();
 
